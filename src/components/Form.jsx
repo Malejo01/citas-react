14e8b06b73cfe3085505dbdaba5b41defb86362f
Malejo01 <lizarragamauroalejandro@gmail.com>
2022-11-22 21:52:37 -0300

    update-agregada alerta de error en el formulario

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,6 +7,7 @@ function Form() {
     const [fecha, setFecha] =useState("")
     const [sintomas, setSintomas] =useState("")
 
+    const[error,setError] = useState(false)
 
     const handleSubmit= (e) =>{
         e.preventDefault();
@@ -15,9 +16,10 @@ function Form() {
 
         if([nombre,propietario,email,fecha,sintomas].includes('')){
             console.log("Hay al menos un campo vacio")
-            
-        }
+            setError(true)
         return;
+        } 
+        setError(false)
     } 
 
     return (
@@ -29,6 +31,9 @@ function Form() {
                 
             <form onSubmit={handleSubmit}
             className='bg-white shadow-xl rounded-lg py-10 px-5 mb-10 ml-10 ' >
+                {error && (<div className='bg-red-800 text-center uppercase text-white p-3 mb-3 rounded font-bold'>
+                                <p>Todos los campos son obligatorios</p>
+                          </div>)}
                 <div className='mb-5'>
                     <label htmlFor='mascota' className='block text-gray-700 uppercase font-bold'>
                         Nombre Mascota
