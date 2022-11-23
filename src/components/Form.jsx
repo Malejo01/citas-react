661737a14c8af44c689f4b7133d012cad8a0c686
Malejo01 <lizarragamauroalejandro@gmail.com>
2022-11-22 21:38:55 -0300

    ya funciona, ahora a agregar notificacion de error

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,7 +7,6 @@ function Form() {
     const [fecha, setFecha] =useState("")
     const [sintomas, setSintomas] =useState("")
 
-    const[error,setError]=useState(false)
 
     const handleSubmit= (e) =>{
         e.preventDefault();
@@ -16,11 +15,10 @@ function Form() {
 
         if([nombre,propietario,email,fecha,sintomas].includes('')){
             console.log("Hay al menos un campo vacio")
-            setError(true)
+            
         }
         return;
     } 
-    setError(false)
 
     return (
         <div className='md:w-1/2 lg:w-2/5 mr-5'>
@@ -31,10 +29,6 @@ function Form() {
                 
             <form onSubmit={handleSubmit}
             className='bg-white shadow-xl rounded-lg py-10 px-5 mb-10 ml-10 ' >
-                {error? <div className='bg-red-800 text-white text-center rounded uppercase font-bold p-3 m-3'>
-                            <p>Todos los campos son obligatorios</p>
-                        </div>
-                    :""}
                 <div className='mb-5'>
                     <label htmlFor='mascota' className='block text-gray-700 uppercase font-bold'>
                         Nombre Mascota
